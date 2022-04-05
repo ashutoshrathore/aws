29938c30b6b414920a31a7b5a140e939a9b4785c
ashutoshrathore <ashutoshrathore.mail@gmail.com>
2022-04-05 16:06:47 +0530

    transferred from old account ashutos-rathore

---
+++ main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+#Required to select source OS
+data "aws_region" "current" {}
+data "aws_partition" "current" {}
+
+
+
+data "aws_vpc" "VPC1" {
+  filter {
+    name   = "tag:Name"
+    values = ["defaultvpc"]
+  }
+}
+
+data "aws_subnet" "SN" {
+  filter {
+    name   = "tag:Name"
+    values = ["default-public-subnet"]
+  }
+}
+
+data "aws_security_group" "SG" {
+  filter {
+    name   = "tag:Name"
+    values = ["default-sec-group*"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "Version" {
+  type    = string
+  default = "3.0.0"
+}
+variable "aws_region" {
+  type    = string
+  default = "us-east-2"
+}
+
+variable "PipelineName" {
+  type    = string
+  default = "ImageBuilderPipe2-TFE"
+}
+variable "RecipeName" {
+  type    = string
+  default = "UbuntuRecipe2"
+}
+variable "InfraName" {
+  type    = string
+  default = "Infra2-TFE"
+}
+variable "DistSetName" {
+  type    = string
+  default = "DistSet2-TFE"
+}
+
+/*
+component_arns = [
+    "arn:aws:imagebuilder:${data.aws_region.current.name}:aws:component/InstallCLI/1.0.0",
+    "arn:aws:imagebuilder:${data.aws_region.current.name}:aws:component/InstallCW/1.0.0"
+  ]
+  */
